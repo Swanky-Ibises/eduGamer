399ebdb1bf26a778111c9e621840210281c81b68
kjng <pianistkevinjang@gmail.com>
2017-02-08 16:34:04 -0800

    Display score

--- client/src/components/GameMemory.js
+++ client/src/components/GameMemory.js
@@ -4,14 +4,16 @@ export default class GameMemory extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      score: 0,
+      cards: [],
+      selectedCards: []
     };
   }
   render() {
     return (
       <div>
         <h2>Memory</h2>
-        <h2>Table goes here</h2>
+        <h3>Score: { this.state.score }</h3>
       </div>
     );
   }
