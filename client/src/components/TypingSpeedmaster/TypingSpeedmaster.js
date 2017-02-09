a8d1c481a33da168cc487ccbda43b85d68c8bf87
kjng <pianistkevinjang@gmail.com>
2017-02-09 13:23:15 -0800

    Create targetText state

--- client/src/components/TypingSpeedmaster/TypingSpeedmaster.js
+++ client/src/components/TypingSpeedmaster/TypingSpeedmaster.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Message, Form, TextArea } from 'semantic-ui-react';
 
 export default class TypingSpeedmaster extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      targetText: 'This is the text you should be typing!'
+    }
+  }
+
+  handleKeypress() {}
+
   render() {
     return (
-      <div>
+      <Message>
         <h1>This is typing speedmaster!</h1>
-      </div>
+        <Message warning>
+          <h3>Enter this text as fast as you can: </h3>
+          {this.state.targetText}
+        </Message>
+        <Form>
+          <TextArea placeholder='user text' />
+        </Form>
+      </Message>
     )
   }
 }
