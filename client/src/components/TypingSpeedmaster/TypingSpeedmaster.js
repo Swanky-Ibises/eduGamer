53a0529e7ef6e267b46a6d250f0c479342751195
kjng <pianistkevinjang@gmail.com>
2017-02-09 15:01:40 -0800

    Complete game logic

--- client/src/components/TypingSpeedmaster/TypingSpeedmaster.js
+++ client/src/components/TypingSpeedmaster/TypingSpeedmaster.js
@@ -4,7 +4,9 @@ import { Message, Form, TextArea } from 'semantic-ui-react';
 export default class TypingSpeedmaster extends React.Component {
   constructor(props) {
     super(props);
+    this.timer = null;
     this.state = {
+      timer: 0,
       targetText: 'This is the text you should be typing!',
       userText: '',
       started: false,
@@ -13,27 +15,51 @@ export default class TypingSpeedmaster extends React.Component {
     this.handleKeypress = this.handleKeypress.bind(this);
   }
 
+  componentWillUpdate() {
+    if (this.state.targetText === this.state.userText && !this.state.done) {
+      this.gameEnd();
+    }
+  }
+
   handleKeypress(event) {
+    if (!this.state.started) {
+      this.startTimer();
+      this.setState({started: true});
+    }
     if (!this.state.done) {
       this.setState({ userText: event.target.value });
     }
   }
 
   gameEnd() {
-    console.log('You win!');
+    this.endTimer();
+    this.setState({done: true});
+    this.submitScore();
   }
 
+  startTimer() {
+    var context = this;
+    this.timer = setInterval(function() {
+      context.setState({timer: context.state.timer + 1});
+    }, 1000);
+  }
+
+  endTimer() {
+    clearInterval(this.timer);
+  }
+
+  submitScore() {}
+
   render() {
-    if (this.state.targetText === this.state.userText) {
-      this.gameEnd();
-    }
     return (
       <Message>
         <h1>This is typing speedmaster!</h1>
+        <h2>Time taken: {this.state.timer}</h2>
         <Message warning>
           <h3>Enter this text as fast as you can: </h3>
           {this.state.targetText}
         </Message>
+        {this.state.done && <h2>You win!!!</h2>}
         <Form>
           <TextArea placeholder='Type here' onChange={this.handleKeypress} />
         </Form>
