e482819bdf47327108e0960b4b83ccaab69f64b9
Josephine-Chen <jychen@g.hmc.edu>
2017-02-08 16:44:05 -0800

    (refactor) Refactored homepage and navbar from bootstrap to semantic

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import { Button } from 'semantic-ui-react';
 
 export default class Homepage extends React.Component {
   render() {
@@ -7,10 +8,7 @@ export default class Homepage extends React.Component {
       <div>
         <h2>Welcome to MasterMind!</h2>
         <h4>MasterMind is a collection of games designed to test and strengthen your mental fortitude.  <br/> While everyone is allowed to play these games,  creating an account gives you the ability to keep track of your personal progress!</h4>
-        <div class="row">
-            <button className="btn btn-default homepageButton"><Link to="/memorygame">Memory</Link></button>
-            <button className="btn btn-default homepageButton"><Link to="/scramblegame">Scramble</Link></button>
-        </div>
+            <Button><Link to="/scramblegame">Scramble</Link></Button>
       </div>
     );
   }
