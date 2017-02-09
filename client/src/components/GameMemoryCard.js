b797fd69c4e40217f10ef29789628e47e361ba20
kjng <pianistkevinjang@gmail.com>
2017-02-08 23:19:42 -0800

    Update card styling

--- client/src/components/GameMemoryCard.js
+++ client/src/components/GameMemoryCard.js
@@ -2,9 +2,9 @@ import { Table } from 'semantic-ui-react';
 
 const GameMemoryCard = (props) => {
   return (
-    <Table.Cell textAlign='center' onClick={() => props.onClick(props.index)}>
+    <Table.Cell negative={!props.card.flipped} textAlign='center' onClick={() => props.onClick(props.index)}>
       {/*<p>{props.index}</p>*/}
-      {props.card.flipped ? props.card.value : ' '}
+      {props.card.flipped ? props.card.value : 'X'}
     </Table.Cell>
   );
 };
