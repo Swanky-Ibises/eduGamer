1a05c1e1552ffc0a57dbf4da0925f3a70b7258e2
kjng <pianistkevinjang@gmail.com>
2017-02-09 12:38:26 -0800

    Put placeholder text for word of the day

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import { Grid, Header, Button, Divider } from 'semantic-ui-react';
+import WordOfTheDay from './WordOfTheDay.js';
 
 export default class Homepage extends React.Component {
   render() {
@@ -9,15 +10,19 @@ export default class Homepage extends React.Component {
         <h1>Welcome to Membrain!</h1>
         <h4>Membrain is a collection of games designed to test and strengthen your mental fortitude.  <br/> While everyone is allowed to play these games,  creating an account gives you the ability to keep track of your personal progress!</h4>
       <Divider section />
-        <Grid columns={2}>
-          <Grid.Column>
-            <Header size='large'>Popular Games</Header>
-            <Link to="/scramblegame"><Button>Scramble</Button></Link>
-            <Link to="/memorygame"><Button>Memory</Button></Link>
-          </Grid.Column>
-          <Grid.Column>
-
-          </Grid.Column>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={2}></Grid.Column>
+            <Grid.Column width={4}>
+              <Header size='large'>Popular Games</Header>
+              <Link to="/scramblegame"><Button>Scramble</Button></Link>
+              <Link to="/memorygame"><Button>Memory</Button></Link>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <WordOfTheDay />
+            </Grid.Column>
+            <Grid.Column width={2}></Grid.Column>
+          </Grid.Row>
         </Grid>
       </div>
     );
--- client/src/components/WordOfTheDay.js
+++ client/src/components/WordOfTheDay.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+export default class WordOfTheDay extends React.Component {
+  render() {
+    return (
+      <Message compact>
+        <Message.Header>Word Here</Message.Header>
+        <Message.Content>Definition of word blah hi there how are you doing andy josephine eric kevin</Message.Content>
+      </Message>
+    );
+  }
+}
