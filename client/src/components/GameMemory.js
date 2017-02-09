22b6841a14cd6cc8f5e402eb0194bc85ca16d02b
kjng <pianistkevinjang@gmail.com>
2017-02-08 16:31:57 -0800

    Add constructor

--- client/src/components/GameMemory.js
+++ client/src/components/GameMemory.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 export default class GameMemory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Memory Game here</h2>
+        <h2>Memory</h2>
+        <h2>Table goes here</h2>
       </div>
     );
   }
