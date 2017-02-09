0134ee430bbdea58f34dc425aeb2be8d4c4fba12
kjng <pianistkevinjang@gmail.com>
2017-02-09 13:16:43 -0800

    Set up main TypingSpeedmaster component

---
+++ client/src/components/TypingSpeedmaster/TypingSpeedmaster.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TypingSpeedmaster extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is typing speedmaster!</h1>
+      </div>
+    )
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,12 @@ import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 import App from './components/App';
 import SignUp from './components/SignUp';
 import Homepage from './components/Homepage';
-import GameMemory from './components/GameMemory';
 import LogIn from './components/Login';
 import {Leaderboard} from './components/Leaderboard';
 import {Profile} from './components/Profile';
 import GameScramble from './components/GameScramble';
+import GameMemory from './components/GameMemory';
+import TypingSpeedmaster from './components/TypingSpeedmaster/TypingSpeedmaster';
 
 const app = document.getElementById('app');
 
@@ -18,10 +19,11 @@ render(
   <Router history={hashHistory}>
     <Route path="/" component = {App}>
       <IndexRoute component={Homepage}></IndexRoute>
+      <Route path="scramblegame" component={GameScramble}/>
       <Route path="memorygame" component={GameMemory}/>
+      <Route path="typingspeedmaster" component={TypingSpeedmaster}/>
       <Route path="leaderboard" component={Leaderboard}/>
       <Route path="profile" component={Profile}/>
-      <Route path="scramblegame" component={GameScramble}/>
       <Route path="login" component={LogIn}/>
       <Route path="signup" component={SignUp}/>
     </Route>
