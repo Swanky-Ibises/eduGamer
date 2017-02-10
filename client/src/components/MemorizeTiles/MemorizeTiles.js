d431180f2c50378ccb1050c3b297d98fc005a519
kjng <pianistkevinjang@gmail.com>
2017-02-10 12:55:56 -0800

    Complete basic version of game

--- client/src/components/MemorizeTiles/MemorizeTiles.js
+++ client/src/components/MemorizeTiles/MemorizeTiles.js
@@ -5,8 +5,9 @@ export default class MemorizeTiles extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      gameStarted: false,
       score: 0,
-      timeLeft: 5,
+      timeLeft: 0,
       startedTimer: false,
       playerPlaying: false,
       gameBoard: [[0,0,0,0],
@@ -14,10 +15,30 @@ export default class MemorizeTiles extends React.Component {
                   [0,0,0,0],
                   [0,0,0,0]],
       playerBoard: [[0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0]]
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0]]
     };
+  }
+
+  componentWillMount() {
+    this.resetTimer();
+  }
+
+  resetTimer() {
+    this.setState({
+      timeLeft: 10,
+      startedTimer: false
+    });
+  }
+
+  resetPlayerBoard() {
+    this.setState({
+      playerBoard: [[0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0],
+                    [0,0,0,0]]
+    })
   }
 
   startGame() {
@@ -33,16 +54,41 @@ export default class MemorizeTiles extends React.Component {
         context.setState({timeLeft: context.state.timeLeft - 1});
       } else {
         clearInterval(timer);
+        context.resetTimer();
         context.setupPlayerBoard();
       }
     }, 1000);
   }
 
   setupPlayerBoard() {
-    this.setState({playerPlaying: true});
+    this.setState({
+      gameStarted: true,
+      playerPlaying: true
+    });
   }
 
-  submitBoard() {}
+  clickTile(rowIndex, tileIndex) {
+    console.log(rowIndex, tileIndex);
+    var board = this.state.playerBoard.slice();
+    board[rowIndex][tileIndex] === 0 ? board[rowIndex][tileIndex] = 1 : board[rowIndex][tileIndex] = 0;
+    console.log(board[rowIndex][tileIndex]);
+    this.setState({
+      playerPlaying: true,
+      playerBoard: board
+    });
+  }
+
+  submitBoard() {
+    // Slow but easy way to compare arrays
+    if (JSON.stringify(this.state.gameBoard) === JSON.stringify(this.state.playerBoard)) {
+      this.setState({score: this.state.score + 1});
+    }
+    this.setState({
+      playerPlaying: false,
+    })
+    this.resetPlayerBoard();
+    this.resetTimer();
+  }
 
   generateGameBoard() {
     var zeroOrOne = function() {
@@ -66,10 +112,13 @@ export default class MemorizeTiles extends React.Component {
         <br />
         <Header size='medium'>Score: {this.state.score}</Header>
         <br />
-        <Header size='medium'>Time left: {this.state.timeLeft} seconds</Header>
-        <Header size='medium'>Select the boxes you just saw...</Header>
+        {!this.state.playerPlaying && this.state.startedTimer && <Header size='medium'>Time left: {this.state.timeLeft} seconds</Header>}
+
+        {this.state.playerPlaying && <Header size='medium'>Select the boxes you just saw...</Header>}
         <br />
-        <Button disabled={this.state.startedTimer} onClick={this.startGame.bind(this)}>Start Game</Button>
+
+        <Button disabled={this.state.startedTimer || this.state.playerPlaying} onClick={this.startGame.bind(this)}>{!this.state.gameStarted ? 'Start Game' : 'Next round'}</Button>
+
         {!this.state.playerPlaying ?
           (<div className='memorize-tile-game'>
             {this.state.gameBoard.map((row, rowIndex) => (
@@ -84,14 +133,14 @@ export default class MemorizeTiles extends React.Component {
             {this.state.playerBoard.map((row, rowIndex) => (
               <div key={rowIndex}>
                 {row.map((tile, tileIndex) => (
-                  <div className={tile === 1 ? 'memorize-tile tile-red' : 'memorize-tile tile-white'} key={tileIndex}></div>
+                  <div className={tile === 1 ? 'memorize-tile tile-red' : 'memorize-tile tile-white'} key={tileIndex} onClick={this.clickTile.bind(this, rowIndex, tileIndex)}></div>
                 ))}
               </div>
             ))}
           </div>)
         }
         <br />
-        <Button disabled={!this.state.playerPlaying}>Submit</Button>
+        <Button disabled={!this.state.playerPlaying} onClick={this.submitBoard.bind(this)}>Submit</Button>
       </Message>
     );
   }
