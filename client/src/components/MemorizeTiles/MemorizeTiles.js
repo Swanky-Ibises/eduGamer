eae4d1e272e10e36d9d2534c0fa3c909c9500692
kjng <pianistkevinjang@gmail.com>
2017-02-10 11:25:48 -0800

    Create basic game board style

--- client/src/components/MemorizeTiles/MemorizeTiles.js
+++ client/src/components/MemorizeTiles/MemorizeTiles.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Message } from 'semantic-ui-react';
+
+// Grid colors:
+//'red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue',
+//'violet', 'purple', 'pink', 'brown', 'grey', 'black'
 
 export default class MemorizeTiles extends React.Component {
   render() {
     return (
-      <Header>Memorize Tiles</Header>
+      <Message>
+        <Header size='huge'>Memorize Tiles</Header>
+        <div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-red'></div>
+        </div>
+        <div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-white'></div>
+        </div>
+        <div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-white'></div>
+          <div className='memorize-tile tile-red'></div>
+        </div>
+        <div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-red'></div>
+          <div className='memorize-tile tile-red'></div>
+        </div>
+      </Message>
     );
   }
 }
