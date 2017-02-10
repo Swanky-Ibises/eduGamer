55a3781bf6dac4ce4f49f7947653b7a5209509b5
kjng <pianistkevinjang@gmail.com>
2017-02-10 11:31:50 -0800

    Add Start Game button placeholder

--- client/src/components/MemorizeTiles/MemorizeTiles.js
+++ client/src/components/MemorizeTiles/MemorizeTiles.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Header, Message } from 'semantic-ui-react';
-
-// Grid colors:
-//'red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue',
-//'violet', 'purple', 'pink', 'brown', 'grey', 'black'
+import { Header, Message, Button } from 'semantic-ui-react';
 
 export default class MemorizeTiles extends React.Component {
   render() {
     return (
       <Message>
         <Header size='huge'>Memorize Tiles</Header>
+        <br />
         <Header size='medium'>Score: </Header>
+        <br />
+        <Button>Start Game</Button>
         <div className='memorize-tile-game'>
           <div>
             <div className='memorize-tile tile-white'></div>
