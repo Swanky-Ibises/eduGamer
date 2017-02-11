9e61c54e009eb36d9783eb96486089e39895a1db
Josephine-Chen <jychen@g.hmc.edu>
2017-02-11 15:08:56 -0800

    Add js to ends of routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 
-import App from './components/App';
-import Homepage from './components/homepage/Homepage';
+import App from './components/App.js';
+import Homepage from './components/homepage/Homepage.js';
 
-import Leaderboard from './components/Leaderboard';
-import Profile from './components/Profile';
+import Leaderboard from './components/Leaderboard.js';
+import Profile from './components/Profile.js';
 
 // Authentication
-import SignUp from './components/authentication/SignUp';
-import LogIn from './components/authentication/Login';
+import SignUp from './components/authentication/SignUp.js';
+import LogIn from './components/authentication/Login.js';
 
 // Games
-import GameScramble from './components/games/Scramble/GameScramble';
-import GameMemory from './components/games/GameMemory/GameMemory';
-import TypingSpeedmaster from './components/games/TypingSpeedmaster/TypingSpeedmaster';
-import MemorizeTiles from './components/games/MemorizeTiles/MemorizeTiles';
-import GameSimon from './components/games/Simon/GameSimon';
-import GameSudoku from './components/games/Sudoku/GameSudoku';
-import GameMastermind from './components/games/Mastermind/GameMastermind';
+import GameScramble from './components/games/Scramble/GameScramble.js';
+import GameMemory from './components/games/GameMemory/GameMemory.js';
+import TypingSpeedmaster from './components/games/TypingSpeedmaster/TypingSpeedmaster.js';
+import MemorizeTiles from './components/games/MemorizeTiles/MemorizeTiles.js';
+import GameSimon from './components/games/Simon/GameSimon.js';
+import GameSudoku from './components/games/Sudoku/GameSudoku.js';
+import GameMastermind from './components/games/Mastermind/GameMastermind.js';
 
 const app = document.getElementById('app');
 
