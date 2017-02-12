9e46669648022a970344321c9560de13cda62a7e
kjng <pianistkevinjang@gmail.com>
2017-02-11 17:19:24 -0800

    Generate random start time

--- client/src/components/Games/ReactionCircle/ReactionCircle.js
+++ client/src/components/Games/ReactionCircle/ReactionCircle.js
@@ -4,6 +4,21 @@ import { Message, Button } from 'semantic-ui-react';
 export default class ReactionCircle extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      milliseconds: 0,
+      seconds: 0,
+      countdown: 0
+    };
+  }
+
+  componentDidMount() {
+    this.generateCountdown();
+  }
+
+  generateCountdown() {
+    // generate random start time 1-4 seconds from now
+    var randomCountdown = Math.ceil(Math.random()*4);
+    this.setState({countdown: randomCountdown});
   }
 
   render() {
@@ -11,10 +26,11 @@ export default class ReactionCircle extends React.Component {
       <Message>
         <h2>Reaction Circle</h2>
         <h3>Test your reaction time...</h3>
-        <h4>Timer: 0.000</h4>
+        <h4>Timer: {this.state.seconds}.{this.state.milliseconds}</h4>
         <div className="reaction-circle">
-          <Button className='start-button'>Start</Button>
+          <Button className='circle-center'>Start</Button>
         </div>
+        <h4>Countdown: {this.state.countdown}</h4>
       </Message>
     );
   }
