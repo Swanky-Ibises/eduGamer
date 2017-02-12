0362b26b9169b31df0bff5bcde98f57116d882eb
kjng <pianistkevinjang@gmail.com>
2017-02-11 16:58:11 -0800

    Create ReactionCircle component

---
+++ client/src/components/Games/ReactionCircle/ReactionCircle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+export default class ReactionCircle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>Reaction Circle</h2>
+      <h4>Test your reaction time...</h4>
+    );
+  }
+}
