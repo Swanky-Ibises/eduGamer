92f45a370668a9108e88e670edbc9a56772920a9
kjng <pianistkevinjang@gmail.com>
2017-02-11 22:46:39 -0800

    Style leaderboard

--- client/src/components/GameLeaderboard.js
+++ client/src/components/GameLeaderboard.js
@@ -5,26 +5,24 @@ import {Header, Table} from 'semantic-ui-react'
 const LeaderboardTable = (props) => {
   let gameHigh = props.game+"High";
   return (
-    <div>
-      <Table basic="very" celled>
+    <div className='leaderboard-table'>
+      <h4>{props.game.charAt(0).toUpperCase() + props.game.slice(1)} Top Scores</h4>
+      <Table celled>
         <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell className="leaderboardHeader">{props.game.charAt(0).toUpperCase() + props.game.slice(1) + " Top Scores"}</Table.HeaderCell>
-          </Table.Row>
           <Table.Row>
             <Table.HeaderCell><span>User</span></Table.HeaderCell>
             <Table.HeaderCell><span>Score</span></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>{props.leaders.map(function(leader, i){
+        <Table.Body>
+        {props.leaders.map(function(leader, i) {
           return (
             <Table.Row key={i}>
               <Table.Cell>{leader.username}</Table.Cell>
-              <Table.Cell>{leader[gameHigh]}</Table.Cell>
+              <Table.Cell>{leader[gameHigh] ? leader[gameHigh] : '0'}</Table.Cell>
             </Table.Row>
             )
-          })
-        }
+          })}
         </Table.Body>
       </Table>
     </div>
